7e8a4e4a9ffccdb3c38f977cf72fa73ac73392f3
hunter <hdowneymac@gmail.com>
2024-12-03 17:59:58 -0600

    Initial Commit

---
+++ FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by hunter downey on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
